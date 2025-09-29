e12df69aafa6d5e837740465bd87bda2b9a93bbd
Ahmad Naimi <naimi.airsh@gmail.com>
2025-09-29 10:34:16 -0400

    minor bug fix

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,30 @@ import Head from "next/head";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <Head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function() {
+                try {
+                  var theme = localStorage.getItem('theme');
+                  if (!theme) {
+                    localStorage.setItem('theme', 'dark');
+                    document.documentElement.classList.add('dark');
+                  } else if (theme === 'dark') {
+                    document.documentElement.classList.add('dark');
+                  }
+                } catch (e) {
+                  document.documentElement.classList.add('dark');
+                }
+              })();
+            `,
+          }}
+        />
       </Head>
       <BackgroundFX />
       <Component {...pageProps} />
